1d7bfac1160895ec844be5c55a367085e4f68165
Robert Haines <rhaines@manchester.ac.uk>
2021-01-10 18:26:18 +0000

    Solution for day 3, part 2.

--- src/bin/spherical_houses.rs
+++ src/bin/spherical_houses.rs
@@ -16,6 +16,12 @@ fn main() {
 
     let visits = visit_houses(directions);
     println!("Part 1: {}", visits.len());
+
+    let (evens, odds) = divide_directions(directions);
+    let santa = visit_houses(evens.as_str());
+    let robo = visit_houses(odds.as_str());
+    let visits = santa.union(&robo).collect::<HashSet<_>>();
+    println!("Part 2: {}", visits.len());
 }
 
 fn visit_houses(directions: &str) -> HashSet<(i32, i32)> {
@@ -42,6 +48,21 @@ fn visit_houses(directions: &str) -> HashSet<(i32, i32)> {
     visits
 }
 
+fn divide_directions(directions: &str) -> (String, String) {
+    let mut s1 = String::new();
+    let mut s2 = String::new();
+
+    for (i, d) in directions.char_indices() {
+        if i % 2 == 0 {
+            s1.push(d);
+        } else {
+            s2.push(d);
+        }
+    }
+
+    (s1, s2)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -59,5 +80,12 @@ mod tests {
     #[should_panic]
     fn bad_directions() {
         visit_houses("<>^va");
+    }
+
+    #[test]
+    fn split_dirs() {
+        let (one, two) = divide_directions("^v^v^v^v");
+        assert_eq!("^^^^", one);
+        assert_eq!("vvvv", two);
     }
 }
