f9a6c4f4a071a487755d3c6eb0667821d21f03ac
Robert Haines <rhaines@manchester.ac.uk>
2021-01-10 23:34:46 +0000

    Solution for day 4, part 2.

--- src/bin/stocking_stuffer.rs
+++ src/bin/stocking_stuffer.rs
@@ -9,16 +9,18 @@
 fn main() {
   let key = "yzbqklnj";
 
-  println!("Part 1: {}", find_suffix(key, 200_000));
+  println!("Part 1: {}", find_suffix(key, 5, 200_000));
+  println!("Part 2: {}", find_suffix(key, 6, 9_900_000));
 }
 
-fn find_suffix(key: &str, start: u32) -> u32 {
+fn find_suffix(key: &str, digits: usize, start: u32) -> u32 {
   let mut i = start;
+  let test = format!("{:0<1$}", "", digits);
 
   loop {
     let digest = md5::compute(key.to_string() + &i.to_string());
 
-    if &format!("{:x}", digest)[0..5] == "00000" {
+    if &format!("{:x}", digest)[0..digits] == test {
       return i;
     }
 
@@ -32,6 +34,6 @@ mod tests {
 
     #[test]
     fn get_suffix() {
-      assert_eq!(609_043, find_suffix("abcdef", 600_000));
+      assert_eq!(609_043, find_suffix("abcdef", 5, 600_000));
     }
 }
