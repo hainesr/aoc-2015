aceea02e90dabb507a3e9106b544b4ff26fefc99
Robert Haines <rhaines@manchester.ac.uk>
2021-01-13 20:08:06 +0000

    Solution for day 5, part 2.

--- src/bin/intern_elves.rs
+++ src/bin/intern_elves.rs
@@ -20,13 +20,14 @@ fn main() {
     let input = input.trim();
 
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn part1(strings: &str) -> u32 {
     let mut num = 0;
 
     for line in strings.lines() {
-        if is_nice_string(line) {
+        if is_nice_string1(line) {
             num += 1;
         }
     }
@@ -34,7 +35,19 @@ fn part1(strings: &str) -> u32 {
     num
 }
 
-fn is_nice_string(text: &str) -> bool {
+fn part2(strings: &str) -> u32 {
+    let mut num = 0;
+
+    for line in strings.lines() {
+        if is_nice_string2(line) {
+            num += 1;
+        }
+    }
+
+    num
+}
+
+fn is_nice_string1(text: &str) -> bool {
     lazy_static! {
         static ref VOWELS: Regex = Regex::new("[aeiou].*[aeiou].*[aeiou]").unwrap();
         static ref DOUBLES: Regex = Regex::new("(.)\\1{1,}").unwrap();
@@ -59,16 +72,43 @@ fn is_nice_string(text: &str) -> bool {
     true
 }
 
+fn is_nice_string2(text: &str) -> bool {
+    lazy_static! {
+        static ref DOUBLE: Regex = Regex::new("(..).*\\1").unwrap();
+        static ref BETWEEN: Regex = Regex::new("(.).\\1").unwrap();
+    }
+
+    match BETWEEN.is_match(text) {
+        Ok(v) => if !v { return false },
+        Err(_) => return false,
+    }
+
+    match DOUBLE.is_match(text) {
+        Ok(v) => if !v { return false },
+        Err(_) => return false,
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn nice_strings() {
-        assert!(is_nice_string("ugknbfddgicrmopn"));
-        assert!(is_nice_string("aaa"));
-        assert!(is_nice_string("haegwjzuvuyypxyu") == false); // Contains 'xy'.
-        assert!(is_nice_string("dvszwmarrgswjxmb") == false); // Only one vowel.
-        assert!(is_nice_string("jchzalrnumimnmhp") == false); // No double letter.
+    fn nice_strings1() {
+        assert!(is_nice_string1("ugknbfddgicrmopn"));
+        assert!(is_nice_string1("aaa"));
+        assert!(is_nice_string1("haegwjzuvuyypxyu") == false); // Contains 'xy'.
+        assert!(is_nice_string1("dvszwmarrgswjxmb") == false); // Only one vowel.
+        assert!(is_nice_string1("jchzalrnumimnmhp") == false); // No double letter.
+    }
+
+    #[test]
+    fn nice_strings2() {
+        assert!(is_nice_string2("qjhvhtzxzqqjkmpb"));
+        assert!(is_nice_string2("xxyxx"));
+        assert!(is_nice_string2("uurcxstgmygtbstg") == false); // No repeat with single letter.
+        assert!(is_nice_string2("ieodomkazucvgmuy") == false); // No pair appearing twice.
     }
 }
